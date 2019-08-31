d0f141b84448a185e21ce6e9582914407308d004
unknown <maldinuribrahim@gmail.com>
2019-08-31 19:42:31 +0700

    Change Search Position

--- resources/views/general/classic/partials/navbar.blade.php
+++ resources/views/general/classic/partials/navbar.blade.php
@@ -11,9 +11,12 @@
         <div class="col-12">
           <div class="d-flex float-lg-left">
             <img class="img-logo" src="{{empty($settings->where('name','global')->flatten()->first()->value['logo']) ? module_asset_url('core:assets/images/Spartan.png') : url($settings->where('name','global')->flatten()->first()->value['logo'])}}" alt="logo">
-            <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <i class="fas fa-bars"></i>
-            </button>
+            <div class="d-flex align-items-center ml-auto">
+	            <a class="nav-link float-left text-white d-lg-none" href="#search-box" data-toggle="collapse"><i class="icofont icofont-search-alt-2"></i></a>
+	            <button class="navbar-toggler text-light" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	              <i class="fas fa-bars"></i>
+	            </button>
+            </div>
           </div>
           <div class="collapse navbar-collapse px-3 px-lg-0" id="navbarSupportedContent">
             <ul class="nav navbar-nav ml-auto">
@@ -33,7 +36,7 @@
                     </li>
                   @endif
                 @endforeach
-                <li><a class="nav-link" href="#search-box" data-toggle="collapse"><i class="icofont icofont-search-alt-2"></i></a></li>
+                <li class="d-none d-lg-block"><a class="nav-link" href="#search-box" data-toggle="collapse"><i class="icofont icofont-search-alt-2"></i></a></li>
             </ul>
           </div>
       </div>
