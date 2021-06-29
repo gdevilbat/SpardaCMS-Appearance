4484d1065e8ff4e646017d00d14b82e8f6d068d4
maldinuribrahim <maldinuribrahim@gmail.com>
2021-06-29 18:08:32 +0700

    Fix 404 Status Error

--- resources/views/general/classic/errors/404.blade.php
+++ resources/views/general/classic/errors/404.blade.php
@@ -10,7 +10,7 @@
     <section>
         <div class="container">
             <div class="d-flex align-item-center justify-content-center">
-                <img src="{{ asset(!empty($settings->where('name','global')->flatten()->first()->value['not_found_image']) ? $settings->where('name','global')->flatten()->first()->value['not_found_image'] : config('app.name')) }}" style="max-width: 100%;height: auto !important" alt="">
+                <img src="{{ asset(!empty($settings->where('name','global')->flatten()->first()->value['not_found_image']) ? generate_storage_url($settings->where('name','global')->flatten()->first()->value['not_found_image']) : config('app.name')) }}" style="max-width: 100%;height: auto !important" alt="">
             </div>
         </div>
     </section>
